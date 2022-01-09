a58aad02d342d24935eaab696fb1438cd3565318
Your Name <mohammadalhamdan94@gmail.com>
2022-01-09 13:42:36 +0300

    task done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react";
+
 
 function App() {
+  const [style, setStyle] = useState("");
+  
+  
+
+  const styles = {
+    bold: { fontWeight: "bold" },
+    italic: { fontStyle: "italic" },
+    underline: { textDecorationLine: "underline" },
+    yellow:{color: "yellow"},
+    blue:{color: "blue"},
+    red:{color: "red"},
+    black:{color: "black"},
+    purple:{color: "purple"}
+  };
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="login-box">
+      
+      <h1 className='h1'> Text Editor</h1>
+      <button className='button' onClick={() => setStyle(style === ""? "bold" :"")}>Bold</button>
+      <button className='button' onClick={() => setStyle(style === ""? "italic":"")}>Italic</button>
+      <button className='button' onClick={() => setStyle(style === ""? "underline":"")}>Underline</button>
+         <div>
+         <textarea  style = {styles[style]} placeholder="Type your text" />
+
+         </div>
+         <div>
+         <button onClick={() => setStyle(style === ""? "yellow" :"")}>Yellow</button>
+         <button onClick={() => setStyle(style === ""? "blue":"")}>Blue</button>
+         <button onClick={() => setStyle(style === ""? "red":"")}>Red</button>
+         <button onClick={() => setStyle(style === ""? "black" :"")}>Black</button>
+         <button onClick={() => setStyle(style === ""? "purple":"")}>Purple</button>
+     
+         </div>
     </div>
   );
 }
